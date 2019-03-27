b8edc450a5360a79ed482b7311e618deabf22fc6
Dimitrius Ionov <dimitrius.ionov@monsanto.com>
2019-03-27 13:21:44 -0700

    add export for TinyYolov2Trainable

--- build/es6/tinyYolov2/index.js
+++ build/es6/tinyYolov2/index.js
@@ -1,4 +1,5 @@
 export * from './TinyYolov2';
 export * from './TinyYolov2Options';
+export * from './TinyYolov2Trainable';
 export * from './config';
 //# sourceMappingURL=index.js.map
--- src/tinyYolov2/index.ts
+++ src/tinyYolov2/index.ts
@@ -1,5 +1,5 @@
-export * from './TinyYolov2';
-export * from './TinyYolov2Options';
-
+export * from './TinyYolov2'
+export * from './TinyYolov2Options'
+export * from './TinyYolov2Trainable'
 export * from './config'
 export * from './types'
